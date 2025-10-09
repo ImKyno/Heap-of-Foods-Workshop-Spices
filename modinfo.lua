04bc4d709380ddb30b20b6d794d48d9447c007bd
John Scott <40477759+ImKyno@users.noreply.github.com>
2025-10-09 19:23:36 -0300

    Preparing The Grounds - Updated Recipes

--- modinfo.lua
+++ modinfo.lua
@@ -1,15 +1,81 @@
-name                        = "Heap of Foods - Warly Spices"
-version                     = "1.4-A"
+local function ChooseTranslationTable(tbl)
+    return tbl[locale] or tbl[1]
+end
 
-description                 = 
+local STRINGS =
+{
+	NAME = 
+	{
+		"Heap of Foods - Warly Spices",
+		zh  = "食物堆积 - 瓦利香料 (Heap of Foods - Warly Spices)",
+		zht = "食物堆積 - 瓦利香料 (Heap of Foods - Warly Spices)",
+		pt  = "Amontoado de Comidas - Temperos do Warly (Heap of Foods - Warly Spices)",
+		pl  = "Stos jedzenia - Przyprawy Warly'ego (Heap of Foods - Warly Spices)",
+		es  = "Montón de Alimentos - Especias de Warly (Heap of Foods - Warly Spices)",
+	},
+	
+	DESCRIPTION =
+	{
 [[
 󰀄 Adds new Warly Spices for the Heap of Foods Mod!
 
 󰀏 This Mod requires that Heap of Foods is enabled. It does nothing on its own. Check Workshop Page for more details.
 
-󰀌 Mod Version: 1.4-A
-󰀧 Update: Seaside Summer (Part II)
-]]
+󰀌 Mod Version: 1.5-A
+󰀧 Update: Preparing The Grounds
+]],
+		zh  =
+[[
+󰀄 为《食物堆积 (Heap of Foods)》模组新增瓦利香料！
+
+󰀏 此模组需要启用《食物堆积 (Heap of Foods)》。它本身没有任何功能。请查看工作坊页面获取更多信息。
+
+󰀌 Mod 版本： 1.5-A
+󰀧 更新内容： 准备地基
+]],
+		zht =
+[[
+󰀄 為《食物堆積 (Heap of Foods)》模組新增瓦利香料！
+
+󰀏 此模組需要啟用《食物堆積 (Heap of Foods)》。它本身沒有任何功能。請查看工作坊頁面以獲取更多資訊。
+
+󰀌 Mod 版本： 1.5-A
+󰀧 更新內容： 準備地基
+]],
+		pt  =
+[[
+󰀄 Adiciona novos Temperos do Warly para o Mod Amontoado de Comidas!
+
+󰀏 Este Mod requer que o Mod Amontoado de Comidas esteja ativado. Ele não faz nada sozinho. Confira a página da Oficina para mais detalhes.
+
+󰀌 Versão do Mod: 1.5-A
+󰀧 Atualização: Preparando o Terreno
+]],
+		pl  =
+[[
+󰀄 Dodaje nowe Przyprawy Warly’ego do modu Stos jedzenia!
+
+󰀏 Ten mod wymaga włączenia Stosu jedzenia. Sam w sobie nic nie robi. Sprawdź stronę warsztatu po więcej informacji.
+
+󰀌 Wersja Moda: 1.5-A
+󰀧 Aktualizacja: Przygotowania do Gruntu
+]],
+		es  =
+[[
+󰀄 ¡Agrega nuevas Especias de Warly para el Mod Montón de Alimentos!
+
+󰀏 Este Mod requiere que Montón de Alimentos esté habilitado. No hace nada por sí solo. Consulta la página del Workshop para más detalles.
+
+󰀌 Versión del Mod: 1.5-A
+󰀧 Actualización: Preparando el Terreno
+]],	
+	},
+}
+
+name                        = ChooseTranslationTable(STRINGS.NAME)
+version                     = "1.5-A"
+
+description                 = ChooseTranslationTable(STRINGS.DESCRIPTION)
 
 author                      = "Kyno"
 api_version                 = 10
@@ -33,33 +99,3 @@ mod_dependencies =
 		workshop = "workshop-2334209327",
 	},
 }
-
--- Brazilian Portuguese
-if locale == "pt" then
-	name                   = "Amontoado de Comidas - Temperos do Warly"
-
-	description            = 
-[[
-󰀄 Adiciona novos Temperos do Warly para o Mod Amontoado de Comidas! 
-
-󰀏 Este Mod requer que o Mod principal esteja ativo. Este Mod não faz nada por si só. Cheque a página da Oficina para mais detalhes.
-
-󰀌 Versão do Mod: 1.4-A
-󰀧 Atualização: Verão à Beira-mar (Parte II)
-]]
-end
-
--- Simplified Chinese.
-if locale == "zh" or locale == "zhr" then
-	name                   = "Heap of Foods - Warly Spices"
-
-	description            = 
-[[
-󰀄 Adds new Warly Spices for the Heap of Foods Mod!
-
-󰀏 This Mod requires that Heap of Foods is enabled. It does nothing on its own.  Check Workshop Page for more details.
-
-󰀌 Mod版本: 1.4-A
-󰀧 Update: Seaside Summer (Part II)
-]]
-end
