669d37b1cc4aa1e5b50be3c349f8401e4150770b
John Scott <40477759+ImKyno@users.noreply.github.com>
2026-01-01 20:00:48 -0300

    Fish Registry - Updated Recipes

--- modinfo.lua
+++ modinfo.lua
@@ -21,7 +21,7 @@ local STRINGS =
 
 󰀏 This Mod requires that Heap of Foods is enabled. It does nothing on its own. Check Workshop Page for more details.
 
-󰀌 Mod Version: 2.2-A
+󰀌 Mod Version: 2.3-A
 󰀧 Update: Heap of Foods 5th Anniversary
 ]],
 		zh  =
@@ -30,7 +30,7 @@ local STRINGS =
 
 󰀏 此模组需要启用《更多料理 (Heap of Foods)》。它本身没有任何功能。请查看工作坊页面获取更多信息。
 
-󰀌 Mod 版本： 2.2-A
+󰀌 Mod 版本： 2.3-A
 󰀧 更新内容： 美食堆五周年
 ]],
 		zht =
@@ -39,7 +39,7 @@ local STRINGS =
 
 󰀏 此模組需要啟用《食物堆積 (Heap of Foods)》。它本身沒有任何功能。請查看工作坊頁面以獲取更多資訊。
 
-󰀌 Mod 版本： 2.2-A
+󰀌 Mod 版本： 2.3-A
 󰀧 更新內容： 美食堆五周年
 ]],
 		pt  =
@@ -48,7 +48,7 @@ local STRINGS =
 
 󰀏 Este Mod requer que o Mod Amontoado de Comidas esteja ativado. Ele não faz nada sozinho. Confira a página da Oficina para mais detalhes.
 
-󰀌 Versão do Mod: 2.2-A
+󰀌 Versão do Mod: 2.3-A
 󰀧 Atualização: 5º Aniversário do Amontoado de Comidas
 ]],
 		pl  =
@@ -57,7 +57,7 @@ local STRINGS =
 
 󰀏 Ten mod wymaga włączenia Stosu jedzenia. Sam w sobie nic nie robi. Sprawdź stronę warsztatu po więcej informacji.
 
-󰀌 Wersja Moda: 2.2-A
+󰀌 Wersja Moda: 2.3-A
 󰀧 Aktualizacja: 5. rocznica Stos jedzenia
 ]],
 		es  =
@@ -66,14 +66,14 @@ local STRINGS =
 
 󰀏 Este Mod requiere que Montón de Alimentos esté habilitado. No hace nada por sí solo. Consulta la página del Workshop para más detalles.
 
-󰀌 Versión del Mod: 2.2-A
+󰀌 Versión del Mod: 2.3-A
 󰀧 Actualización: Montón de Alimentos 5º Aniversario
 ]],	
 	},
 }
 
 name                        = ChooseTranslationTable(STRINGS.NAME)
-version                     = "2.2-A"
+version                     = "2.3-A"
 
 description                 = ChooseTranslationTable(STRINGS.DESCRIPTION)
 
